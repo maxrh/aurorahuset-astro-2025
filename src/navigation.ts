237ca692c4dffe048352fdfea3aad6ec7eb00c44
Max Hargreave <layout@monsun.dk>
2025-11-07 21:26:16 +0100

    clean up, mobile fix, hero bg illu

--- src/navigation.ts
+++ src/navigation.ts
@@ -31,11 +31,11 @@ export const footerData = {
         {
             title: '',
             links: [
-                { text: 'Om AuroraHuset', href: '/om-aurorahuset/' },
-                { text: 'Om Opholdet', href: '/ophold/' },
-                { text: 'Kontakt', href: '/kontakt/' },
-                { text: 'Cookiepræferencer', href: '#' },
-                { text: 'Privatlivspolitik', href: getPermalink('/privacy') },
+                { text: 'Om AuroraHuset', href: getPermalink('/om-aurorahuset/') },
+                { text: 'Om Opholdet', href: getPermalink('/ophold/') },
+                { text: 'Kontakt', href: getPermalink('/kontakt/') },
+                // { text: 'Cookiepræferencer', href: '#' },
+                { text: 'Privatlivspolitik', href: getPermalink('/privatlivspolitik') },
             ],
         },
     
@@ -44,11 +44,11 @@ export const footerData = {
     // { text: 'Privatlivspolitik', href: getPermalink('/privacy') },
   ],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
+    // { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
+    // { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
+    // { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
     { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/arthelokyo/astrowind' },
+    // { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/arthelokyo/astrowind' },
   ],
   footNote: `
     © ${new Date().getFullYear()} Aurora Women ApS · Alle rettigheder forbeholdes · Website af <a class="hover:text-primary font-semibold transition duration-200 ease-in-out" href="https://monsun.dk"> MONSUN</a>
